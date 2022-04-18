98fb3fd62de361ddc4237eac0f334dcedc80249a
ds <dskim.logiall@gmail.com>
2022-04-18 23:33:33 +0900

    test: 게임 테스트

---
+++ src/test/java/baseball/model/GameTest.java
@@ -0,0 +1,28 @@
+package baseball.model;
+
+import baseball.model.ball.AutoBallGenerator;
+import baseball.model.ball.BallNumber;
+import baseball.model.ball.InputBallGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @DisplayName("play 테스트")
+    @Test
+    public void play() {
+        //given
+        Player defender = new Player(new AutoBallGenerator());
+        Player offender = new Player(new AutoBallGenerator());
+
+        Game game = new Game(offender, defender);
+        Hint hint = game.play();
+
+        //then
+        assertThat(hint).isNotNull();
+    }
+
+}
